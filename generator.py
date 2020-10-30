92979f4e51f18fea9c200d4c8c25111bd8c80633
Damian <damianhgn@gmail.com>
2020-10-30 15:57:32 -0500

    Generate a 2D list and initialize to 0. This is the foundation of the puzzle.

---
+++ generator.py
@@ -0,0 +1,17 @@
+import test_functions
+
+# initialize each row with 0 as a place holder
+row1 = [0 for i in range(9)]
+row2 = [0 for i in range(9)]
+row3 = [0 for i in range(9)]
+row4 = [0 for i in range(9)]
+row5 = [0 for i in range(9)]
+row6 = [0 for i in range(9)]
+row7 = [0 for i in range(9)]
+row8 = [0 for i in range(9)]
+row9 = [0 for i in range(9)]
+
+# create a  2D list to form a grid.
+# NOTE: Coordinates are backwards Y,X
+grid = [row1, row2, row3, row4, row5, row6, row7, row8, row9]
+
